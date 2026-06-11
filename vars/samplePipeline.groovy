555b7aee6be2bce2e2f6177b14b48fee73e6bd33
Your Name <you@example.com>
2026-06-11 10:30:57 +0530

    k8

---
+++ vars/samplePipeline.groovy
@@ -0,0 +1,74 @@
+def call(Map configMap){
+    pipeline {
+        agent {
+            label 'agent1'
+        }
+        enviornment {
+            course = 'jenkins'
+        }
+        options {
+                timeout(time: 30, unit: 'MINUTES') 
+            disableConcurrentBuilds()
+        }
+        parameters {
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+            text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+            booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+            choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+            password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password') 
+        }
+        // Build
+        stages {
+            stage ('build') {
+                steps {
+                    script {
+                        sh """
+                        echo "hello build"
+                        sleep 10
+                        env
+                        echo "Hello ${params.PERSON}
+                        """
+                    }
+                }
+            }
+            stage ('test') {
+                steps {
+                    script {
+                        echo 'building'
+                    }
+                }
+            }
+            stage('Deploy') {
+                input {
+                    message "Should we continue?"
+                    ok "Yes, we should."
+                    submitter "alice,bob"
+                    parameters {
+                        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                    }
+                }
+                steps {
+                    script{
+                        echo "Hello, ${PERSON}, nice to meet you."
+                        
+                        echo 'Deploying..'
+                    }
+                }
+            }
+            
+        }
+
+        post {
+            always {
+                echo 'not completed'
+                deleteder()    
+            }
+            success {
+                echo 'sucess'
+            }
+            failure {
+                echo 'fail'
+            }
+        }
+    }
+}
