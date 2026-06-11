8ce1a4bf2801f556db54b3eab53986e6b8e67c6a
Your Name <you@example.com>
2026-06-11 11:08:53 +0530

    k8

--- vars/samplePipeline.groovy
+++ vars/samplePipeline.groovy
@@ -5,6 +5,7 @@ def call(Map configMap){
         }
         environment {
             course = 'jenkins'
+            greeting = configMap.get('greeting')
         }
         options {
                 timeout(time: 30, unit: 'MINUTES') 
@@ -14,8 +15,6 @@ def call(Map configMap){
             string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
             text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
             booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
-            choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
-            password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password') 
         }
         // Build
         stages {
